86d151155fd47c8aeb633e0216d6561a22d9d32d
ZhuolS <ZlsMB@ZlsMB.local>
2017-06-10 09:28:26 -0700

    add the derived class--heuristic borad

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
+#include<stack>
+#include<vector>
+#include<list>
+#define N 8
+
 using namespace std;
 
+typedef struct chess_moves {
+   // 'x' and 'y' coordinates on chess board
+   int x,y;
+}chess_moves;
 
-template < typename value >
+static  chess_moves move_KT[8] = { {2,1},{1,2},{-1,2},{-2,1},
+                              {-2,-1},{-1,-2},{1,-2},{2,-1} };
+
+
 class board_base{
 public:
-   board_base( size_t n, size_t m, value T)
-      :nRow(n), nCol(m), board(std::vector<std::vector<value>>(n,std::vector<value>(m, T))) {
-         size_t n = nn;
+      //default constructor
+   board_base( size_t n, size_t m, size_t T)
+      :nRow(n), nCol(m), count(0), board(std::vector<std::vector<size_t>>(n,std::vector<size_t>(m, T))) {
+     /*    size_t n = nn;
          size_t m = mm;
          if( n < m ){
             n = mm;
@@ -15,37 +28,188 @@ public:
          }
          if ( ((m%2)==1 && (n%2)==1) || (m==1) || (m==2) || (m==4) || (m == 3 && (n == 4 || n == 6 || n == 8 )))
             std::cout << " Knight's Tour is not possible"
+      }*/
+      //destructor
+      }
+   virtual ~board_base(){}
+      //copy constructor
+   void printboard() {
+   for (size_t i = 0; i < nRow; i++) {
+      for (size_t j = 0; j < nCol; j++) {
+          std::cout<<board[i][j]<<"\t";
+      }
+      std::cout<<endl;
+      }
+   }
+
+   virtual bool move(const chess_moves& move) = 0;
+
+protected:
+   std::vector<std::vector<size_t>> board;
+
+   
+
+   size_t nRow;
+   size_t nCol;
+   size_t count;
+   
+};
+
+
+class record_board : public board_base {
+public:
+   record_board(size_t nn, size_t mm, size_t T=0)
+      :board_base(nn,mm,T){}
+
+   bool move(const chess_moves& move){
+      
+      board[move.x][move.y] = count;
+      ++count;
+      return true;
+   }
+
+   
+
+private:
+   
+
+};
+
+
+class heuristic_board : public board_base {
+
+public:
+
+   heuristic_board(size_t nn=N, size_t mm=N)
+      :board_base(nn,mm,8){
+
+        for ( size_t i = 0; i < nn; ++i){
+         for ( size_t j = 0; j < mm; ++j){
+            if(i == 0 || i == nn-1) {
+               if (j == 0 || j == mm-1)
+                     board[i][j] = 2;
+               else if (j == 1 || j== mm-2)
+                     board[i][j] = 3;
+               else   
+                     board[i][j] = 4;
+               }
+            else if(i == 1 || i == nn-2) {
+               if (j == 0 || j == mm-1)
+                     board[i][j] = 3;
+               else if (j == 1 || j==mm-2)       
+                     board[i][j] = 4;
+               else
+                     board[i][j] = 6;
+
+               }
+            else{
+               if (j == 0 || j==mm-1)
+                     board[i][j] = 4;
+               else if ( j == 1 || j == mm-2)   
+                     board[i][j] = 6;
+               
+               }
+
+            }
+         }
+        }
+    
+   
+      
+
+   virtual ~heuristic_board(){}
+
+   bool move(const chess_moves& move){
+
+      if(!isMovePossible(move)){
+         std::cout<<" move not possible";
+         return false;
+      }
+     
+      board[move.x][move.y] = 0;
+      ++count;
+
+      chess_moves next_move;
+      std::list<chess_moves> temp;
+      std::list<size_t> heuristic;
+      for (size_t i = 0; i < 8; ++i) {
+      // get the next move
+         next_move.x = move.x + move_KT[i].x;
+         next_move.y = move.y + move_KT[i].y;
+   
+         if (isMovePossible(next_move)) {
+            temp.push_back(next_move);
+            board[next_move.x][next_move.y] = board[next_move.x][next_move.y] + 1;
+            heuristic.push_back(board[next_move.x][next_move.y]);
+         }
+      }
+      std::list<chess_moves>::iterator it1 = temp.begin();
+      std::list<chess_moves>::iterator it2 = temp.begin();
+      std::list<size_t>::iterator it3 = heuristic.begin();
+      std::list<size_t>::iterator it4 = heuristic.begin();
+      while(!temp.empty()){
+         auto fn = [&](size_t& T)->void{
+            if(T > *it3){
+               it1 = it2;
+               it3 = it4;
+            }
+            ++it2;
+            ++it4;
+         };
+         std::for_each(heuristic.begin(),heuristic.end(),fn);
+         depth.push(*it1);
+         temp.erase(it1);
+         heuristic.erase(it3);
+         it1 = temp.begin();
+         it2 = temp.begin();
+         it3 = heuristic.begin();
+         it4 = heuristic.begin();
       }
 
-   ~board_base(){}
-
-   board_base(const board_base& old){
-      nRow = old.nRow;
-      nCol = old.nCol;
-      board = old.board;
+      return true;
    }
 
-   board_base& operator= (board_base old){
-      std::swap(nRow, old.nRow);
-      std::swap(nCol, old.nCol);
-      std::swap(board, old.nRow);
-      return *this;
+   void back_track( const chess_moves& move){
+
+
+      chess_moves next_move;
+      size_t num(0);
+
+      for (size_t i = 0; i < 8; ++i) {
+      // get the next move
+         next_move.x = move.x + move_KT[i].x;
+         next_move.y = move.y + move_KT[i].y;
+         
+         if (isMovePossible(next_move)) {
+            
+            board[next_move.x][next_move.y] = board[next_move.x][next_move.y] + 1;
+            ++num;
+         }
+      }
+      board[move.x][move.y] = num;
+      --count;
+
    }
-
-
+      // check if the next move (as per knight's constraints) is possible
    bool isMovePossible(const chess_moves& move) {
       int i = move.x;
       int j = move.y;
-      if ((i >= 0 && i < nRow) && (j >= 0 && j < nCol) && (board[i][j] == 0))
+      if ((i >= 0 && i < nRow) && (j >= 0 && j < nCol) && (board[i][j] != 0 && count < nCol*nRow))
          return true;
       return false;
-}
-
+   }
 
 private:
-   size_t nRow;
-   size_t nCol;
-   std::vector < std::vector<value> > board;
+   std::stack<chess_moves> depth;
+
 };
 
+
+
+// main
+int main() {
+   heuristic_board heuristic;
+   heuristic.printboard();
+   return 0;
+}
 
